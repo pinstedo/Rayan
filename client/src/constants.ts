ae4753a4737688be1c6f2c81932a1a2bd2b56afa
pinstedo <mhdfasaltld@gmail.com>
2026-03-29 22:08:27 +0530

    constant url upadated

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,4 +1,4 @@
 // Replace localhost with your computer's IP if running on a physical device
 // For Android Emulator, use 'http://10.0.2.2:5000/api'
-export const API_URL = 'http://192.168.1.8:5000/api';
+export const API_URL = 'rayan-production-e5e9.up.railway.app';
 
