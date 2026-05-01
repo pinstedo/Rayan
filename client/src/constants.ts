2a28c9f7d33440b54d9f3785dc89095f306bffa4
pinstedo <mhdfasaltld@gmail.com>
2026-05-01 22:27:30 +0530

    generation speed up, dated advance

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,5 +1,5 @@
 // Replace localhost with your computer's IP if running on a physical device
 // For Android Emulator, use 'http://10.0.2.2:5000/api'
-//export const API_URL = 'https://rayan-ooi0.onrender.com/api';
-export const API_URL = 'http://localhost:5000/api';
+export const API_URL = 'https://rayan-ooi0.onrender.com/api';
+// export const API_URL = 'http://localhost:5000/api';
 
